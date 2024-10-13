cef090b3c61e2c57a7e6a7e2ae465c6f276fd2dd
mozesster <169057086+mozesster@users.noreply.github.com>
2024-10-13 10:18:14 +0300

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -21,7 +21,7 @@ resource "aws_db_instance" "default" {
   username             = "foo"
   password             = "foobarbaz"
   parameter_group_name = "default.mysql5.7"
-  publicly_accessible  = false
+  publicly_accessible  = true
 }
 
 
